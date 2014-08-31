396e10dfc157b759e57bb13d2b41d6633e822140
Ruiyuan Li <lry198010@gmail.com>
2014-08-31 18:15:11 +0800

    constructor method in Sequence, some more sequence relative function, redefined StatRecord

--- lib/ReadSequence/Sequence.java
+++ lib/ReadSequence/Sequence.java
@@ -1,24 +1,103 @@
 package ReadSequence;
 import ReadSequence.Exception.*;
+import Stat.StatRecord;
 public class Sequence {
-  public Sequence(String name, String seq, String qual) throws BaseQualNumberNotEqualException {
-    seq = seqQualTrim(seq);
-    qual = seqOrQual(qual);
-    if (seq.length() != qual.length()) {
-       throws new BaseQualNumberNotEqualException(name,seq,qual);
+  public Sequence(String name, String seq, String qual, int phredOffset) throws BaseQualNumberNotEqualException, QuailOffsetNegativeException {
+    this.seq = seq.trim();
+    this.qual = qual.trim();
+    if (this.seq.length() != this.qual.length()) {
+       throws new BaseQualNumberNotEqualException(name,this.seq,this.qual);
     } 
     this.name = name;
-    this.seq = seq;
-    this.qual = qual;
+    if(phredOffset < 0) {
+      throw new QuailOffsetNegativeException(phredOffset);
+    }
+    this.phredOffset = phredOffset; // Sanger
+    begin = 0;
+    end = this.seq.length();
+    seqType = 0;
   }  
   
-  public static String seqQualTrim(String seqOrQual){
-     
+  public Sequence(String name, String seq, String qual) throws BaseQualNumberNotEqualException, QuailOffsetNegativeException {
+    this(name,seq,qual,33);  
   }
+
+  // for fasta format
+  public Sequence(String name, String seq) throws BaseQualNumberNotEqualException, QuailOffsetNegativeException { // for Fast
+    this(name,seq,seq.replaceAll(".","I"),33);
+    seqType = 1;
+  }
+
+  public Sequence(Sequence seqRecord, int forwardClipEnd, int reverseClipBegin) throws BaseQualNumberNotEqualException, QuailOffsetNegativeException {
+    this(seqRecord.getName(), seqRecord.getSeq(), seqRecord.getQual(),seqRecord.getPhredOffset());
+    records[recordIndex++] = new StatRecord("ByInitial",forwardClipEnd,forwardClipEnd,reverseClipBegin,seqRecord.getSeq().length() - reverseClipBegin + 1);
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getRawSeq(){
+    return seq;
+  }
+
+  public String getRawQual(){
+    return qual;
+  }
+  
+  public boolean isEmpty(){
+    if(seq == null) return true;
+    if(begin >= end) return true;
+    return false;
+  }
+  
+  public String getSeq(){
+    if (isEmpty()) return null;
+    return seq.subString(begin,end);
+  }
+  
+  public String getQual(){
+    if (isEmpty()) return null;
+    return qual.subString(begin,end);
+  }  
+
+  public int[] getQualNumber(){
+    if (isEmpty()) return null;
+    String qual = getQual();
+    int[] qualDigit = new int[qual.length()];
+    for(int i = 0; i < qualDigit.length; i++){
+      qualDigit[i] = (int)(qual.charAt(i)) - phredOffset;
+    }
+    return qualDigit;
+  }
+  
+  public void setPhredOffset(int offset){
+    phredOffset = offset;
+  }
+  
+  public int getPhredOffset(){
+    return phredOffset;
+  }
+  
+  public int getSeqType(){
+    return seqType;
+  }  
+
+  public void setSeqType(int seqType){
+    this.seqType = seqType ;
+  }  
+  
+  public double getGC(){
+    
+  }
+ 
   private String name;
   private String seq;
   private String qual;
   private int begin; 
   private int end;
-  
+  private StatRecord[] records = new StatRecord[20];
+  private int recordIndex = 0;
+  private int phredOffset = 33;
+  private int seqType; // 0:fastq, 1:fasta
 }
--- lib/Stat/StatRecord.java
+++ lib/Stat/StatRecord.java
@@ -1,26 +1,30 @@
 package Stat;
 public class StatRecord {
-  public StatRecord(String type, int start, int end){
+  public StatRecord(String type, int forwardClipEnd, int forwardClipLength, int reverseClipBegin, int reverseClipLength){
     recordType = type;
-    clipStart = start; 
-    clipEnd = end;
+    forwardEnd = forwardClipEnd;
+    forwardLength = forwardClipLength;
+    reverseBegin = reverseClipBegin
+    reverseLength = reverseClipLength;
   } 
   
-  public int basesClipped(int seqStart, int seqEnd){
+  public int basesClipped(){
     //return (clipStart - seqStart > 0 ? clipStart - seqStart : 0) +
     //       (seqEnd - clipEnd > 0? seqEnd - clipEnd : 0);
-    return basesClippedForward(seqStart) + basesClippedReverse(seqEnd);
+    return basesClippedForward() + basesClippedReverse();
   }
   
-  public int basesClippedForward(int seqStart){
-    return clipStart - seqStart > 0 ? clipStart - seqStart : 0;
+  public int basesClippedForward(){
+    return forwardLength;
   }
 
-  public int basesClippedReverse(int seqStart){
-    return seqEnd - clipEnd > 0? seqEnd - clipEnd : 0;
+  public int basesClippedReverse(){
+    return reverseLength;
   }
 
   private String recordType;
-  private int clipStart;
-  private int clipEnd;
+  private int forwardEnd;
+  private int forwardLength = 0;
+  private int reverseBegin;
+  private int reverseLength = 0;
 }
