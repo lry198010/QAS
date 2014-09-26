a43f0cb0c325dcd85f0368846598078b32f24eab
Ruiyuan Li <lry198010@gmail.com>
2014-09-26 12:01:05 +0800

    add leadtrimer.java

---
+++ lib/Screen/LeadTrimmer.java
@@ -0,0 +1,15 @@
+package Screen;
+
+import Sequence.Sequence;
+import Sequence.Exception.*;
+import static java.lang.Math.ceil;
+
+public class LeadTrimmer extends Trimmer {
+  public LeadTrimmer(int len){
+    
+  }
+  
+  public int screen(Sequence seq){
+
+  }
+} 
--- lib/Screen/Trimmer.java
+++ lib/Screen/Trimmer.java
@@ -1,8 +1,35 @@
+package Screen;
+
+import Sequence.Sequence;
+import Sequence.Exception.*;
+import static java.lang.Math.ceil;
 public abstract class Trimmer implements Screener {
   public abstract int screen(Sequence seq);
-  public abstract long getBasesScreened();
-  public abstract long getReadsScreened();
-  public abstract long getBasesTrimmed(); 
-  public abstract long getReadsTrimmed(); 
+  public long getBasesScreened(){
+    return totalBasesScreened; 
+  }
+  public long getReadsScreened(){
+    return totalReadsScreened;
+  }
+  public long getBasesTrimmed(){ 
+    return totalBasesTrimmed;
+  }
+  public long getReadsTrimmed(){ 
+    return totalReadsTrimmed; 
+  }
   
+  public getBasesTrimRation(Trimmer[] trimmers){
+    long totalBasesTrimmed = 0;
+    long totalBasesScreened = 0;
+    for(Trimmer T : trimmers){
+      totalBasesTrimmed += T.getBasesTrimmed();
+      totalBasesScreened += T.getBasesScreened(); 
+    }
+    return ((double)totalBasesTrimmed)/totalBasesScreened;
+  }
+  
+  protected long totalBasesScreened = 0;
+  protected long totalReadsScreened = 0;
+  protected long totalBasesTrimmed = 0;
+  protected long totalReadsTrimmed = 0; 
 } 
