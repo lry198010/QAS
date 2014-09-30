9793d376e5e0c6c1db121d07dcab1595c7f19a33
Ruiyuan Li <lry198010@gmail.com>
2014-09-30 17:36:55 +0800

    begin leadtrimmer.java

--- lib/Screen/LeadTrimmer.java
+++ lib/Screen/LeadTrimmer.java
@@ -6,10 +6,11 @@ import static java.lang.Math.ceil;
 
 public class LeadTrimmer extends Trimmer {
   public LeadTrimmer(int len){
-    
+    lenTrimmed = len; 
   }
   
   public int screen(Sequence seq){
-
+    
   }
+  protected int lenTrimmed = 0;
 } 
